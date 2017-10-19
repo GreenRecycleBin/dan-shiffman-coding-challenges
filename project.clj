3efb2d6a6a2913693d3e0ef11be3319e7406d21f
Daniel Le <greenrecyclebin@gmail.com>
2017-10-19 15:09:48 +0800

    Add starfield/core.cljs and starfield.html

--- project.clj
+++ project.clj
@@ -4,4 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.6.0"]])
+                 [org.clojure/clojurescript "1.9.946"]
+                 [quil "2.6.0"]]
+
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :hooks [leiningen.cljsbuild]
+
+  :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js"]
+
+  :cljsbuild
+  {:builds [{:source-paths ["src/cljs/dan_shiffman_coding_challenges/starfield"]
+             :compiler
+             {:output-to "resources/public/js/starfield.js"
+              :output-dir "resources/public/js/starfield"
+              :asset-path "js/starfield"
+              :main "dan-shiffman-coding-challenges.starfield.core"
+              :parallel-build true}}]})
