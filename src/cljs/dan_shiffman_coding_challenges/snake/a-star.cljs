742bf03db6a42584af775476522eaa7309a320a0
Daniel Le <greenrecyclebin@gmail.com>
2018-03-21 15:49:24 +0800

    Combine cell-to-parent-cell, cell-to-direction and score together

--- src/cljs/dan_shiffman_coding_challenges/snake/a-star.cljs
+++ src/cljs/dan_shiffman_coding_challenges/snake/a-star.cljs
@@ -15,20 +15,24 @@
 (defn- heuristic-cost [[x1 y1 :as start] [x2 y2 :as goal]]
   (manhattan-distance x1 y1 x2 y2))
 
-(defn- directions [cell-to-parent-cell cell-to-direction {:food-x :x :food-y :y :as food}]
-  (let [path (take-while identity (iterate #(cell-to-parent-cell %) [food-x food-y]))]
-    (reverse (map #(cell-to-direction %) (drop-last path)))))
+(defn- directions [cell-to-parent-cell-and-direction-and-score
+                   {:food-x :x :food-y :y :as food}]
+  (let [path
+
+        (take-while identity
+                    (iterate #(first (cell-to-parent-cell-and-direction-and-score %)) [food-x food-y]))]
+    (reverse (map #(nth (cell-to-parent-cell-and-direction-and-score %) 1)
+                  (drop-last path)))))
 
 (defn a* [{:keys [x y scale direction tail] :as snake}
           {:food-x :x :food-y :y :as food}]
   (loop [closed (transient #{})
          open-snake (priority-map snake 0)
-         cell-to-parent-cell (transient {})
-         cell-to-direction (transient {})
-         score (transient {[x y] 0})]
+         cell-to-parent-cell-and-direction-and-score (transient {[x y] [nil nil 0]})]
     (when-let [[{:keys [x y scale direction tail] :as snake}] (peek open-snake)]
       (if (= [x y] [food-x food-y])
-        (directions (persistent! cell-to-parent-cell) (persistent! cell-to-direction) food)
+        (directions (persistent! cell-to-parent-cell-and-direction-and-score)
+                    food)
 
         (let [neighbors
               (neighbors snake #{(opposite-direction direction)} closed)
@@ -36,21 +40,19 @@
               neighbor-to-new-score
 
               (for [{:new-x :x :new-y :y :as neighbor} neighbors
-                    :let [new-score (+ (get score [x y]) scale (heuristic-cost [new-x new-y] [food-x food-y]))]
-                    :when (< new-score (get score [new-x new-y] js/Number.MAX_SAFE_INTEGER))]
+                    :let [new-score
+
+                          (+ (nth (get cell-to-parent-cell-and-direction-and-score [x y]) 2)
+                             scale
+                             (heuristic-cost [new-x new-y] [food-x food-y]))]
+                    :when (< new-score
+                             (or (nth (get cell-to-parent-cell-and-direction-and-score [new-x new-y]) 2)
+                                 js/Number.MAX_SAFE_INTEGER))]
                 [neighbor new-score])]
           (recur (conj! closed [x y])
                  (into (pop open-snake) neighbor-to-new-score)
 
-                 (reduce #(conj! %1 %2) cell-to-parent-cell
-                         (for [[{:new-x :x :new-y :y}] neighbor-to-new-score]
-                           [[new-x new-y] [x y]]))
-
-                 (reduce #(conj! %1 %2) cell-to-direction
-                         (for [[{:new-x :x :new-y :y :direction :direction}] neighbor-to-new-score]
-                           [[new-x new-y] direction]))
-
-                 (reduce #(conj! %1 %2) score
-                         (for [[{:new-x :x :new-y :y} score]
+                 (reduce #(conj! %1 %2) cell-to-parent-cell-and-direction-and-score
+                         (for [[{:new-x :x :new-y :y :direction :direction} score]
                                neighbor-to-new-score]
-                           [[new-x new-y] score]))))))))
+                           [[new-x new-y] [[x y] direction score]]))))))))
