ea9bd3c0dde877aa6512d2926a4c98f82afda1b2
Daniel Le <greenrecyclebin@gmail.com>
2018-03-21 15:06:24 +0800

    Add ^boolean type hints

--- src/cljs/dan_shiffman_coding_challenges/snake/snake.cljs
+++ src/cljs/dan_shiffman_coding_challenges/snake/snake.cljs
@@ -16,7 +16,7 @@
    :right {:x-speed 1 :y-speed 0 :next-direction :right}})
 
 (defrecord Snake [x y tail count
-                  x-speed y-speed direction next-direction moving? dead?
+                  x-speed y-speed direction next-direction ^boolean moving? ^boolean dead?
                   width height scale]
 
   Renderable
@@ -42,7 +42,7 @@
                                  (assoc :direction next-direction)
                                  (assoc :next-direction nil)))
 
-          {:keys [x y tail x-speed y-speed moving? dead? width height scale]} m]
+          {:keys [x y tail x-speed y-speed ^boolean moving? ^boolean dead? width height scale]} m]
       (if (and moving? (not dead?))
         (let [new-x (+ x (* scale x-speed))
               new-y (+ y (* scale y-speed))
